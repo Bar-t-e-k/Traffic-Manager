f1fa4c40e9f10fb42e6d3871b42c02e4a8ff58ac
Bartek <132946708+Bar-t-e-k@users.noreply.github.com>
2026-06-12 18:53:54 +0200

    Enhance Android dashboard UI and implement NDK logging
    * Refactor `TrafficViewModel` to track packet statistics and maintain a history of `TrafficLog` objects.
    * Update `MainActivity` with a dashboard layout featuring status cards, a scrollable log list, and a `TopAppBar` with clear actions.
    * Modify the C++ `Logger` to use `__android_log_print` when running on Android for better NDK debugging.

--- core/Traffic-Manager/logger.hpp
+++ core/Traffic-Manager/logger.hpp
@@ -6,9 +6,18 @@
 #include <string>
 #include <ctime>
 
+#ifdef __ANDROID__
+#include <android/log.h>
+#define LOG_TAG "TrafficManager-C++"
+#endif
+
 class Logger {
 public:
     static void log(const std::string& message) {
+
+#ifdef __ANDROID__
+        __android_log_print(ANDROID_LOG_INFO, LOG_TAG, "%s", message.c_str());
+#else
         std::cout << message << "\n";
 
         std::ofstream file("simulator_logs.txt", std::ios::app);
@@ -16,17 +25,18 @@ public:
             std::time_t now = std::time(nullptr);
             char timeStr[26];
 
-            #ifdef _WIN32
-                ctime_s(timeStr, sizeof(timeStr), &now);
-            #else
-                ctime_r(&now, timeStr);
-            #endif
+#ifdef _WIN32
+            ctime_s(timeStr, sizeof(timeStr), &now);
+#else
+            ctime_r(&now, timeStr);
+#endif
 
             std::string t(timeStr);
             if (!t.empty() && t.back() == '\n') t.pop_back();
 
             file << "[" << t << "] " << message << "\n";
         }
+#endif
     }
 };
 
